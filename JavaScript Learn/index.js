1dc2f9406f16caa8bb15198960062bdf71eb02cb
Kumar Sahil <2270168@kiit.ac.in>
2023-04-05 09:48:20 +0530

    Learning Javascript

---
+++ JavaScript Learn/index.js
@@ -0,0 +1,19 @@
+// 1.ways to print in javascript
+//console.log("Hello World Kumar sahil", 4 + 6, "Another log");
+//alert("me")
+//document.write("this is  document")
+
+// 2.JavaScript Console API
+//console.log("Hello World sahil")
+//console.warn("this is a warning");
+//console.error("this is an error");
+
+// 3. Java Script Variables
+// for commenting more than one line at once press cntrl + /
+// Multi 
+// line 
+// comment
+
+var number1 = 34;
+var number2 = 44;
+console.log(number1 + number2);
